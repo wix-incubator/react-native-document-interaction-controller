5abed2959d1a5aeea6b995d54f4cf5da3ad58ac2
Aaron Greenwald <aarong@wix.com>
2016-07-05 13:48:21 +0300

    Initial Commit

---
+++ RNDocumentInteractionController/RNDocumentInteractionController.h
@@ -0,0 +1,13 @@
+//
+//  RNDocumentInteractionController.h
+//  RNDocumentInteractionController
+//
+//  Created by Aaron Greenwald on 7/5/16.
+//  Copyright © 2016 Wix.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNDocumentInteractionController : NSObject
+
+@end
---
+++ RNDocumentInteractionController/RNDocumentInteractionController.m
@@ -0,0 +1,13 @@
+//
+//  RNDocumentInteractionController.m
+//  RNDocumentInteractionController
+//
+//  Created by Aaron Greenwald on 7/5/16.
+//  Copyright © 2016 Wix.com. All rights reserved.
+//
+
+#import "RNDocumentInteractionController.h"
+
+@implementation RNDocumentInteractionController
+
+@end
